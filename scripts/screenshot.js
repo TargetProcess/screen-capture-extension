8a1a25434af3d62c8e3c3c0cc69b34b3df3a70f3
Vladimir Petriko <vlad@targetprocess.com>
2014-03-14 19:24:36 +0300

    retina is not supported for now

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -13,7 +13,8 @@ function setScreenshotUrl(url) {
     var canvasId = 'imageView';
 
     var fCanvas = new fabric.Canvas(canvasId, {
-        selection: true,
+        selection: false,
+        skipTargetFind: true,
         perPixelTargetFind: true,
         targetFindTolerance: 5
     });
@@ -22,7 +23,7 @@ function setScreenshotUrl(url) {
         var w = img.getWidth();
         var h = img.getHeight();
 
-		var dpxRatio = window.devicePixelRatio;
+        var dpxRatio = window.devicePixelRatio;
         img.setWidth(w/dpxRatio);
         img.setHeight(h/dpxRatio);
 
@@ -31,60 +32,8 @@ function setScreenshotUrl(url) {
             height: h / dpxRatio
         });
 
-        // var dpxRatio = window.devicePixelRatio;
-
-        // var xw = w * dpxRatio;
-        // var xh = h * dpxRatio;
-
-        // var c = fCanvas.getElement();
-
-        // fCanvas.setDimensions({
-        //     width: w / dpxRatio,
-        //     height: h / dpxRatio
-        // });
-
-        // // Scale the canvas up by two for retina
-        // c.setAttribute('width', xw);
-        // c.setAttribute('height', xh);
-
-        // // finally set the scale of the context
-        // c.getContext('2d').scale(dpxRatio, dpxRatio);
-
         fCanvas.setBackgroundImage(img, fCanvas.renderAll.bind(fCanvas));
 
         g_ready.resolve(fCanvas);
     });
-
-/*
-    var r = getRatio();
-    var canvas = document.getElementById('imageView');
-
-    var image = new Image();
-    image.onload = function() {
-        var w = image.width / r;
-        var h = image.height / r;
-
-        canvas.width = w;
-        canvas.height = h;
-        var context = canvas.getContext('2d');
-        var scaleRatio = 1 / r;
-
-        context.save();
-        context.scale(scaleRatio, scaleRatio);
-        context.drawImage(image, 0, 0);
-        context.restore();
-
-        // Add the temporary canvas.
-        var tmpCanvas = canvas.cloneNode();
-        tmpCanvas.id = 'imageTemp';
-        tmpCanvas.width = w;
-        tmpCanvas.height = h;
-        canvas.parentNode.appendChild(tmpCanvas);
-
-        g_ready.resolve();
-
-        image.onload = null;
-    };
-    image.src = url;
-*/
 }
